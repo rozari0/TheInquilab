b3cc0adf768493b57031953d012d006429aea1da
root <root@DESKTOP-7TUA87A>
2024-07-16 23:22:26 +0600

    Update Bootstrap url.

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("badmin96/", admin.site.urls),
     path("", include("newslist.urls")),
 ]
