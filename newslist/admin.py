b14ded5ddebdc24a43702540edca86022a73c915
rozari0 <61970262+rozari0@users.noreply.github.com>
2024-08-09 15:47:45 +0600

    Add admin action.

--- newslist/admin.py
+++ newslist/admin.py
@@ -9,17 +9,29 @@ class DescriptionInline(admin.StackedInline):
     model = LongDescription
 
 
+@admin.action(description="Mark selected as approved")
+def make_approved(modeladmin, request, queryset):
+    queryset.update(approved=True)
+
+
+@admin.action(description="Mark selected as not approved")
+def make_not_approved(modeladmin, request, queryset):
+    queryset.update(approved=False)
+
+
 class MartyrAdmin(admin.ModelAdmin):
     list_display = ("name", "id", "approved")
     list_filter = ("approved",)
     inlines = [
         DescriptionInline,
     ]
+    actions = [make_approved, make_not_approved]
 
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ("title", "id", "approved")
     list_filter = ("approved", "is_killed")
+    actions = [make_approved, make_not_approved]
 
 
 admin.site.register(News, NewsAdmin)
